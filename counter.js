1b4b8dc0d562390a5f9d2776cc769d42173188ed
BeratOk <beratcoc23@gmail.com>
2019-10-29 16:06:16 +0300

    Back To Top butonu eklendi. Header da düzeltmeler yapıldı.
    Back To Top butonu eklendi. Header da düzeltmeler yapıldı.

---
+++ counter.js
@@ -0,0 +1,9 @@
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script>
+    var app = new Vue({
+        el: '#counter',
+        data: {
+            counter: 0,
+        }
+    })
+</script>
